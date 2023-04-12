c03b7b796209c344a341a17efe607122da50109d
ikjo93 <auddlr100@naver.com>
2023-04-13 00:49:16 +0900

    fix : 리프레쉬 토큰 아규먼트 리졸버 애노테이션 이름 수정

--- src/main/java/com/pullanner/auth/jwt/argumentresolver/RefreshToken.java
+++ src/main/java/com/pullanner/auth/jwt/argumentresolver/RefreshToken.java
@@ -1,4 +1,4 @@
-package com.pullanner.auth.argumentresolver;
+package com.pullanner.auth.jwt.argumentresolver;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface LoginUser {
+public @interface RefreshToken {
 
 }
