305758c776668217c636bba47995c1f2be7c86cc
ikjo93 <auddlr100@naver.com>
2023-04-18 01:30:28 +0900

    fix : OAuth2 회원가입 시 닉네임 이메일로 자동 부여

--- src/main/java/com/pullanner/global/auth/oauth2/dto/OAuthAttributes.java
+++ src/main/java/com/pullanner/global/auth/oauth2/dto/OAuthAttributes.java
@@ -42,14 +42,10 @@ public class OAuthAttributes {
     public User toEntity() {
         return User.builder()
             .name(name)
-            .nickName(getNickNameFromEmail(email))
+            .nickName(email)
             .email(email)
             .picture(picture)
             .role(Role.GUEST)
             .build();
-    }
-
-    private static String getNickNameFromEmail(String email) {
-        return email.substring(0, email.lastIndexOf('@'));
     }
 }
