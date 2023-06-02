82a65c89b3f4fc411308ef54e857e46b82ce1e4c
ikjo93 <auddlr100@naver.com>
2023-06-03 01:26:30 +0900

    fix : 사용자 계정 도용 감지 시 응답 메시지 수정

--- src/main/java/com/pullanner/global/api/ApiResponseCode.java
+++ src/main/java/com/pullanner/global/api/ApiResponseCode.java
@@ -9,7 +9,7 @@ public enum ApiResponseCode {
 
     OAUTH2_LOGIN_FAIL("A02", 401, "로그인이 실패했습니다."),
     TOKEN_INVALID("A03", 403, "유효하지 않은 토큰입니다."),
-    TOKEN_HACKED("A04", 401, "토큰 도용이 의심됩니다."),
+    TOKEN_HACKED("A04", 401, "계정이 도용된 것으로 의심됩니다."),
     TOKEN_REFRESHED("A05", 200, "액세스 토큰이 재발급되었습니다."),
 
     USER_NOT_FOUND("U01", 404, "존재하지 않는 회원입니다."),
