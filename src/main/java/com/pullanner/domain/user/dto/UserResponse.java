6b50bb62e3565f00d303a0e02d460948c173e1e9
ikjo93 <auddlr100@naver.com>
2023-06-23 19:51:07 +0900

    feat : User 회원정보 데이터 수정

--- src/main/java/com/pullanner/domain/user/dto/UserResponse.java
+++ src/main/java/com/pullanner/domain/user/dto/UserResponse.java
@@ -14,6 +14,7 @@ public class UserResponse {
     private String profileImage;
     private String oauthProvider;
     private Integer level;
+    private Integer journalCount;
 
     @Builder
     private UserResponse(Long userId, String name, String nickName, String email,
