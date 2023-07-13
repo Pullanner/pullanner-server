5a5a04f9da8bf784392782e7a8ebdbb099656688
ikjo93 <auddlr100@naver.com>
2023-07-14 01:54:22 +0900

    feat : access token & refresh token 전송하기 위한 swagger 설정 추가

--- src/main/java/com/pullanner/global/config/SwaggerConfig.java
+++ src/main/java/com/pullanner/global/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.pullanner.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +24,36 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI pullannerOpenAPI() {
+        Info info = new Info()
+            .title("Pullanner Open API")
+            .description("Pullanner 프로젝트 API 명세서")
+            .version("v0.0.1");
+
+        String accessTokenSchemeName = "access token";
+        String refreshTokenSchemeName = "refresh token";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList(accessTokenSchemeName);
+        securityRequirement.addList(refreshTokenSchemeName);
+
+        Components components = new Components();
+        components.addSecuritySchemes(accessTokenSchemeName, new SecurityScheme()
+            .name(accessTokenSchemeName)
+            .type(Type.HTTP)
+            .scheme("beared")
+            .bearerFormat("JWT")
+        );
+
+        components.addSecuritySchemes(refreshTokenSchemeName, new SecurityScheme()
+            .name(refreshTokenSchemeName)
+            .type(Type.APIKEY)
+            .in(In.COOKIE)
+            .name("renew")
+        );
+
         return new OpenAPI()
-            .info(new Info()
-                    .title("Pullanner Open API")
-                    .description("Pullanner 프로젝트 API 명세서")
-                    .version("v0.0.1")
-            );
+            .info(info)
+            .addSecurityItem(securityRequirement)
+            .components(components);
     }
 }
