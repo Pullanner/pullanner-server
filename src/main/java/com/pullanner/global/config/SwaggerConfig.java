ebb958d93868fad565a1ab51f64a04d0e5e9ad9d
ikjo93 <auddlr100@naver.com>
2023-07-13 22:05:18 +0900

    feat : swagger 설정 추가

---
+++ src/main/java/com/pullanner/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.pullanner.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("v1-definition")
+            .pathsToMatch("/api/**")
+            .build();
+    }
+
+    @Bean
+    public OpenAPI pullannerOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                    .title("Pullanner Open API")
+                    .description("Pullanner 프로젝트 API 명세서")
+                    .version("v0.0.1")
+            );
+    }
+}
