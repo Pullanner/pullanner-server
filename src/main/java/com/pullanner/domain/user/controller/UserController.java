20b6442fc817eb10cbc70ca2b223cdc963be2ebb
ikjo93 <auddlr100@naver.com>
2023-07-14 02:07:08 +0900

    feat : Swagger 를 이용한 User API 문서화를 위한 코드 작성

--- src/main/java/com/pullanner/domain/user/controller/UserController.java
+++ src/main/java/com/pullanner/domain/user/controller/UserController.java
@@ -7,6 +7,13 @@ import com.pullanner.domain.user.service.UserService;
 import com.pullanner.global.api.ApiResponseCode;
 import com.pullanner.global.api.ApiResponseMessage;
 import com.pullanner.global.auth.jwt.argumentresolver.RefreshTokenId;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.pullanner.global.api.ApiUtil.getResponseEntity;
 
+@Tag(name = "user", description = "사용자 API")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -25,30 +33,83 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "사용자 정보 조회", description = "사용자 정보를 조회하는 기능입니다.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
     @GetMapping("/api/users")
     public UserResponse find(@AuthenticationPrincipal Long userId) {
         return userService.findById(userId);
     }
 
+    @Operation(summary = "사용자 닉네임 중복 검사", description = "사용자 닉네임의 중복 여부를 검사하는 기능입니다.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
+    @Parameter(name = "nickname", description = "사용자 닉네임", example = "ikjo")
     @GetMapping("/api/users/duplicate")
-    public ResponseEntity<ApiResponseMessage> validate(@RequestParam @Length(min = NICKNAME_MIN_LENGTH, max = NICKNAME_MAX_LENGTH) String nickname) {
+    public ResponseEntity<ApiResponseMessage> validate(
+        @RequestParam @Length(min = NICKNAME_MIN_LENGTH, max = NICKNAME_MAX_LENGTH) String nickname) {
         return userService.validateDuplicate(nickname);
     }
 
+    @Operation(summary = "사용자 닉네임 등록(수정)", description = "사용자 닉네임을 등록하거나 수정할 수 있는 기능입니다.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
     @PostMapping("/api/users")
-    public UserResponse register(@AuthenticationPrincipal Long userId, @Valid @RequestBody UserUpdateRequest userInfo) {
+    public UserResponse register(
+        @AuthenticationPrincipal Long userId,
+        @Valid @RequestBody @Parameter(name = "nickname", description = "사용자 닉네임") UserUpdateRequest userInfo) {
         return userService.register(userId, userInfo);
     }
 
+    @Operation(summary = "사용자 이메일 인증 코드 발송", description = "사용자의 회원 탈퇴 요청을 처리하기 위해 사용자 이메일로 인증 코드를 발송하는 기능입니다.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
     @PostMapping("/api/users/mail")
     public ResponseEntity<ApiResponseMessage> mail(@AuthenticationPrincipal Long userId) {
         userService.sendMail(userId);
         return getResponseEntity(ApiResponseCode.USER_EMAIL_SENDING_SUCCESS);
     }
 
+    @Operation(summary = "사용자 이메일 인증 코드 발송", description = "사용자의 회원 탈퇴 요청을 처리하기 위해 사용자 이메일로 인증 코드를 발송하는 기능입니다.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "401", description = "Invalid Auth Code", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
+    @Parameter(name = "인증 코드", description = "회원 탈퇴 처리를 위한 인증 코드", example = "123456")
     @DeleteMapping("/api/users")
-    public ResponseEntity<ApiResponseMessage> delete(@AuthenticationPrincipal Long userId,
-        @RefreshTokenId String refreshTokenId, @RequestParam Integer code) {
+    public ResponseEntity<ApiResponseMessage> delete(
+        @AuthenticationPrincipal Long userId,
+        @RefreshTokenId String refreshTokenId,
+        @RequestParam Integer code) {
         userService.deleteUser(userId, refreshTokenId, code);
         return getResponseEntity(ApiResponseCode.USER_DELETED_SUCCESS);
     }
