28dc8614233a0c79ee9c1fe60a2b32f48ba00abd
ikjo93 <auddlr100@naver.com>
2023-08-30 00:35:31 +0900

    feat : Plan 생성 API swagger 기능 추가

--- src/main/java/com/pullanner/web/controller/plan/PlanController.java
+++ src/main/java/com/pullanner/web/controller/plan/PlanController.java
@@ -4,6 +4,12 @@ import com.pullanner.web.ApiResponseCode;
 import com.pullanner.web.ApiResponseMessage;
 import com.pullanner.web.controller.plan.dto.PlanSaveOrUpdateRequest;
 import com.pullanner.web.service.plan.PlanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.pullanner.web.ApiUtil.getResponseEntity;
 
+@Tag(name = "Plan", description = "Plan API")
+@ApiResponses(
+        {
+                @ApiResponse(responseCode = "401", description = "INVALID AUTHENTICATION REQUEST", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+                @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class))),
+                @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class)))
+        }
+)
 @RequiredArgsConstructor
 @RestController
 public class PlanController {
 
     private final PlanService planService;
 
+    @Operation(summary = " 조회", description = "사용자가 철봉 운동 계획을 등록할 수 있는 기능입니다.")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ApiResponseMessage.class)))
     @PostMapping("/api/plans")
     public ResponseEntity<ApiResponseMessage> register(
             @AuthenticationPrincipal Long userId,
