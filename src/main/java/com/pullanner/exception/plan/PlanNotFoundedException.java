257db20f420ae42f27819aa22b8f10cd608b85a7
ikjo93 <auddlr100@naver.com>
2023-09-05 22:17:50 +0900

    feat : Plan 관련 예외 클래스 추가

--- src/main/java/com/pullanner/exception/plan/PlanNotFoundedException.java
+++ src/main/java/com/pullanner/exception/plan/PlanNotFoundedException.java
@@ -4,7 +4,7 @@ import com.pullanner.web.ApiResponseCode;
 
 public class PlanNotFoundedException extends RuntimeException {
 
-    public PlanNotFoundedException(String message) {
-        super(message);
+    public PlanNotFoundedException(Long planId) {
+        super("식별 번호가 " + planId + "에 해당되는 계획이 없습니다.");
     }
 }
