0e8461c8527ac721ade0df21b1096425e952285e
ikjo93 <auddlr100@naver.com>
2023-09-05 22:45:48 +0900

    feat : 사용자 경험치 상승 정책 enum 구현

---
+++ src/main/java/com/pullanner/domain/user/ExperiencePolicy.java
@@ -0,0 +1,23 @@
+package com.pullanner.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExperiencePolicy {
+
+    JOIN(10),
+    PLAN_COMPLETED(2),
+    BADGE_ALL_ROUNDER(80),
+    BADGE_MUSCLE_KING(30),
+    BADGE_EXPERIENCE_KING(30),
+    BADGE_PULL_UP_KING(100),
+    BADGE_FIRST_PLAN(10),
+    BADGE_ONE_HUNDRED_PLAN(100),
+    BADGE_SEVEN_SEQUENCE_PLAN_COMPLETED(70),
+    BADGE_COMMUNICATION_KING(10),
+    BADGE_REACTION_KING(10);
+
+    private final int point;
+}
