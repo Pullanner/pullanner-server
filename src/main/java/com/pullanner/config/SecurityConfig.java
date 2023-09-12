8a317ecdb0b6a40df68d88815803c01c2cae2440
ikjo <82401504+ikjo93@users.noreply.github.com>
2023-09-12 22:46:39 +0900

    fix : configure cors(http method)

--- src/main/java/com/pullanner/config/SecurityConfig.java
+++ src/main/java/com/pullanner/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://localhost:5173", System.getenv("CLIENT_URL")));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.addAllowedHeader("*");
         // Cross Origin 에 요청을 보낼 때 요청에 인증(credential) 정보를 담아서 보낼 수 있는지 결정하는 항목
         configuration.setAllowCredentials(true);
