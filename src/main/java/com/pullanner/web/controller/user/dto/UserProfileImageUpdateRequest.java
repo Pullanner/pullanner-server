dd4e9935afb3b15b6f5a6406bce9395af151b78e
ikjo93 <auddlr100@naver.com>
2023-09-21 01:10:19 +0900

    fix : Add Setter and NoArgsConstructor of UserProfileImageUpdateRequest

--- src/main/java/com/pullanner/web/controller/user/dto/UserProfileImageUpdateRequest.java
+++ src/main/java/com/pullanner/web/controller/user/dto/UserProfileImageUpdateRequest.java
@@ -3,10 +3,14 @@ package com.pullanner.web.controller.user.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserProfileImageUpdateRequest {
 
     @NotNull
