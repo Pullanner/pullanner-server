5e4d24a79ca6d8998ff07ddd2b919a8c9db12b8d
ikjo93 <auddlr100@naver.com>
2023-11-08 19:11:07 +0900

    fix : 운동 시간대별 완료된 철봉 운동 계획 개수 조회 API 기능 수정

--- src/main/java/com/pullanner/domain/plan/PlanCompletedTimeEnum.java
+++ src/main/java/com/pullanner/domain/plan/PlanCompletedTimeEnum.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 @Getter
 public enum PlanCompletedTimeEnum {
 
-    MORNING("morning"), AFTER_NOON("afternoon"), EVENING("evening"), NIGHT("night");
+    NIGHT("night"), MORNING("morning"), AFTER_NOON("afternoon"), EVENING("evening");
 
     private final String time;
 
